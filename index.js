bb5ffa4fb909768eadc664630f25a5a43ee9c6ac
Viktor Gorosh <gorosh.viktor@gmail.com>
2020-09-10 20:09:28 +0600

    Первая версия, одинаковые квадраты

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 // Style guide: элементы, начинающиеся с # - типа Node
 let $start = document.getElementById('start');
 let $game = document.getElementById('game');
+let score = 0;
 
 $start.addEventListener('click', startGame);
+$game.addEventListener('click', handleBoxClick);
 
 function startGame() {
 	$start.classList.add('hide');
@@ -13,5 +15,22 @@ function startGame() {
 }
 
 function renderBox() {
+	$game.innerHTML = '';
 	
+	let box = document.createElement('div');
+	
+	box.style.width = box.style.height = '50px';
+	box.style.position = 'absolute';
+	box.style.top = '70px';
+	box.style.left = '70px';
+	box.style.backgroundColor = '#000';
+	box.setAttribute('data-box', 'true');
+	
+	$game.append(box);
+}
+
+function handleBoxClick(event) {
+	if (!event.target.dataset.box) return;
+	score++;
+	renderBox();
 }
