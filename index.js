7e18920ed9842baed79d3a0a9dbca7565bf486bd
Viktor Gorosh <gorosh.viktor@gmail.com>
2020-09-10 22:20:14 +0600

    Добавил таймер

--- index.js
+++ index.js
@@ -3,15 +3,34 @@
 // Style guide: элементы, начинающиеся с # - типа Node
 let $start = document.getElementById('start');
 let $game = document.getElementById('game');
+let $time = document.getElementById('time');
+
+let isGameStarted = false;
 let score = 0;
 
 $start.addEventListener('click', startGame);
 $game.addEventListener('click', handleBoxClick);
 
 function startGame() {
+	isGameStarted = true;
 	$start.classList.add('hide');
 	$game.style.backgroundColor = '#fff';
 	renderBox();
+	
+	let interval = setInterval(function () {
+		let time = parseFloat($time.textContent);
+		
+		if (time <= 0) {
+			clearInterval(interval);
+			endGame();
+		} else {
+			$time.innerHTML = (time - 0.1).toFixed(1);
+		}
+	}, 100);
+}
+
+function endGame() {
+	isGameStarted = false;
 }
 
 function renderBox() {
@@ -34,7 +53,7 @@ function renderBox() {
 }
 
 function handleBoxClick(event) {
-	if (!event.target.dataset.box) return;
+	if (!isGameStarted || !event.target.dataset.box) return;
 	score++;
 	renderBox();
 }
