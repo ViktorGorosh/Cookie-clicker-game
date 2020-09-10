e9c13e3a8adb10e1509ac15dd072f549e97d1f43
Viktor Gorosh <gorosh.viktor@gmail.com>
2020-09-10 21:57:40 +0600

    Реализовал случайные размеры и координаты мишени

--- index.js
+++ index.js
@@ -18,11 +18,15 @@ function renderBox() {
 	$game.innerHTML = '';
 	
 	let box = document.createElement('div');
+	let boxSize = getRandom(30, 100);
+	let gameSize = $game.getBoundingClientRect();
+	let maxTop = gameSize.height - boxSize;
+	let maxLeft = gameSize.width - boxSize;
 	
-	box.style.width = box.style.height = '50px';
+	box.style.width = box.style.height = boxSize + 'px';
 	box.style.position = 'absolute';
-	box.style.top = '70px';
-	box.style.left = '70px';
+	box.style.top = getRandom(0, maxTop) + 'px';
+	box.style.left = getRandom(0, maxLeft) + 'px';
 	box.style.backgroundColor = '#000';
 	box.setAttribute('data-box', 'true');
 	
@@ -33,4 +37,8 @@ function handleBoxClick(event) {
 	if (!event.target.dataset.box) return;
 	score++;
 	renderBox();
+}
+
+function getRandom(min, max) {
+	return Math.floor( min + Math.random() * (max - min) );
 }
